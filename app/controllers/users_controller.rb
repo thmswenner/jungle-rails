e716544504c67624b80e1fd4b2b41a18d5079da0
Thomas Wenner <thmswenner@gmail.com>
2019-05-16 15:50:52 +0000

    Create user_controller & session_controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      session[:user_id] = user_id
+      redirect_to '/'
+    else
+      redirect_to 'signup'
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password_digest, :password_confirmation)
+    end
+
+end
