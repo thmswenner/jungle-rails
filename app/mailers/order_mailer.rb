9c0bb1aed1d4f58201f0931bdd859748731d95df
Thomas Wenner <thmswenner@gmail.com>
2019-05-17 02:21:04 +0000

    Create HTML to contain information from the orders database

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,6 +4,7 @@ class OrderMailer < ApplicationMailer
   def order_email(order)
     @fauxemail = "thmswenner@gmail.com"
     @order = order
+    @total = order.total
     @url = 'http://example.com/login'
     mail(to: @fauxemail, subject:'Thank you for your order')
   end
